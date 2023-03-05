3e7e3757ac9a98df9796bfb8fb9bd5924536af70
katoj65 <katoj652gmail.com>
2023-03-05 14:51:47 +0300

    added endpoint test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(20)->create();
+        User::factory(20)->create();
         $this->call([UserWeatherFocustSeeder::class]);
     }
 }
